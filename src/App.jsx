56f1b0ea2a36e9e37bf2c1d2ce265ce5050ff4cf
ignacio <ignaciofranciscovigo@gmail.com>
2025-01-13 16:12:41 -0300

    Ex 7.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/name";
+
+// Custom hook
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
+// Custom hook
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState(null);
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) return;
 
-  return country
-}
+    axios
+      .get(`${baseUrl}/${name}`)
+      .then((res) => setCountry(res.data))
+      .catch((error) => {
+        setCountry({ error: error.response.data.error });
+      });
+  }, [name]);
 
+  return country;
+};
+
+// Country Component
 const Country = ({ country }) => {
   if (!country) {
-    return null
+    return null;
   }
 
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+  if (country.error) {
+    return <div>{country.error}</div>;
   }
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.name.official} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div>
+      <img
+        src={country.flags.png}
+        height="100"
+        alt={`flag of ${country.name.official}`}
+      />
     </div>
-  )
-}
+  );
+};
 
+// Main component
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
 
   const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+    e.preventDefault();
+    // This change in state should cause the useCountry to rerun
+    setName(nameInput.value);
+  };
 
   return (
     <div>
@@ -65,7 +81,7 @@ const App = () => {
 
       <Country country={country} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
